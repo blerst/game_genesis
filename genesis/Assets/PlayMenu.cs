092d18b490911ad37966ec6e9b77746dba8373d6
Rahul <blastyplayz@gmail.com>
2022-05-25 11:38:10 +1000

    Imported Game
    Haha yeah

---
+++ genesis/Assets/PlayMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+    public void Back ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Easy ()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Normal ()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Hard ()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
